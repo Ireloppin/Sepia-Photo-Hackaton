170c2dd40de664c9294af063a13773c42159486a
Irene Lopez <elcorreodeirene@gmail.com>
2023-02-23 08:41:17 +0100

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+  import Cloudinary from './Cloudinary.svelte';
+ import StepUpload from './StepUpload.svelte';
+ import StepEdit from './StepEdit.svelte';
+  import { imageStatus } from './store';
+  import {ImageStatus} from '../types.d'
+</script>
+
+
+<div class="max-w-xl m-auto grid grid-cols-1 place-content-center w-full h-screen p-4">
+<header class="flex justify-center py-10">
+  <h1 class="text-3xl font-bold text-blue-900 tracking-tighter">Sepia<span class="text-blue-600">Photo</span></h1>
+</header>
+<main class="w-full block">
+  {#if $imageStatus === ImageStatus.READY || $imageStatus === ImageStatus.UPLOADING}
+  <StepUpload />
+  {:else if $imageStatus === ImageStatus.DONE}
+  <StepEdit />
+  {/if}
+
+</main>
+
+<footer class="flex justify-center items-center gap-x-2 font-semibold pt-10" >
+  Hecho con <a href='https://cloudinary' target="blank" rel="noreferrer"><Cloudinary /></a>
+</footer>
+</div>
---
+++ src/StepEdit.svelte
@@ -0,0 +1,30 @@
+ <script lang="ts">
+    import {originalImage, modifiedImage} from './store'
+    import "two-up-element"
+
+    /* let proccessingImage =true
+    let tries = 0
+    let intervalId: any
+
+    $:{
+        if(proccessingImage){
+            clearInterval(intervalId)
+            intervalId = setInterval(()=>{
+                tries++
+            }, 500)
+        }
+    } */
+ </script>
+
+<!-- on:load={()=>(proccessingImage = false)}
+on:error={()=>(proccessingImage = true)}  -->
+
+<two-up>
+ <img src={$originalImage} alt="Imagen subida por el usuario"/>
+ <img 
+  
+ src={`${$modifiedImage}`} alt="Imagen editada"/>
+</two-up>
+<div class="text-center w-full mt-8">
+<a download href={$modifiedImage} class="bg-blue-500 hover:bg-blue-700 text-xl font-bols text-white rounded-full px-4 py-2 ">Descargar imagen editada</a>
+</div>
